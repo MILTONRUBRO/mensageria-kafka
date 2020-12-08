f7e8bf587a90d25d62339d621752b77c50bf57a7
miltonrubro@gmail.com <miltonrubro@gmail.com>
2020-12-07 22:44:48 -0300

    Criando producer da mensageria

---
+++ src/main/java/br/com/devmos/ecommercekafka/NewOrderMain.java
@@ -0,0 +1,40 @@
+package br.com.devmos.ecommercekafka;
+
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+public class NewOrderMain {
+	
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
+		 KafkaProducer<String, String> producer = new KafkaProducer<>(properties());
+		 
+		 String key = "123";
+		 String value = "132123, 67543, 7897533";
+		 
+		 ProducerRecord<String, String> record = new ProducerRecord<>("ECOMMERCE_NEW_ORDER", key, value);
+		 
+		 producer.send(record, (data, ex) ->{
+			 if(ex != null) {
+				 ex.printStackTrace();
+				 return;
+			 }
+			 System.out.println("Sucesso enviando " +data.topic() + ":::partition " + data.partition() + "/ offset " +data.offset());
+			 
+		 }).get();
+	}
+	
+	private static Properties properties() {
+		Properties properties = new Properties();
+		properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
+		properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+		return properties;
+	}
+
+}
