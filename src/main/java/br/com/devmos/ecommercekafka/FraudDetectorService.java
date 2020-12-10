f318ce333b70fdb27490239467c71f767524ea48
miltonrubro@gmail.com <miltonrubro@gmail.com>
2020-12-10 20:53:59 -0300

    Adicionando id

--- src/main/java/br/com/devmos/ecommercekafka/FraudDetectorService.java
+++ src/main/java/br/com/devmos/ecommercekafka/FraudDetectorService.java
@@ -3,6 +3,7 @@ package br.com.devmos.ecommercekafka;
 import java.time.Duration;
 import java.util.Collections;
 import java.util.Properties;
+import java.util.UUID;
 
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
@@ -53,7 +54,8 @@ public class FraudDetectorService {
 		properties.setProperty(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "127.0.0.1:9092");
 		properties.setProperty(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
 		properties.setProperty(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
-		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getName());
+		properties.setProperty(ConsumerConfig.GROUP_ID_CONFIG, FraudDetectorService.class.getSimpleName());
+		properties.setProperty(ConsumerConfig.CLIENT_ID_CONFIG, FraudDetectorService.class.getSimpleName() + "_" + UUID.randomUUID().toString());
 
 		return properties;
 	}
